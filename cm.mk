210901930fbf3dc113ad184b4ee564db7294ec41
Jeffrey Clark <dude@zaplabs.com>
2012-08-22 07:59:02 -0500

    new bootanimation does not require height

--- cm.mk
+++ cm.mk
@@ -11,7 +11,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.1/JRO03H/UCLF6:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.1 JRO03H UCLF6 release-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-800x1280
+TARGET_BOOTANIMATION_NAME := vertical-800
 
 PRODUCT_NAME := cm_quincyatt
 PRODUCT_DEVICE := quincyatt
